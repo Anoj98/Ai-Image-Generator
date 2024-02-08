94efda50bd9943b465946b61b82284b91d880418
Anoj <anojws@gmail.com>
2024-02-08 23:30:01 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import { BrowserRouter, Link, Route, Router} from 'react-router-dom'
+
+
+// import { Home, CreatePost } from './pages'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hellog world!</h1>
+    </div>
+  );
+};
+
+export default App;
